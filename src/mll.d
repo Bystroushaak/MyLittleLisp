68d288eb8852ec85a037b3e905f8d934b6b0d7c3
Bystroushaak <bystrousak@kitakitsune.org>
2012-06-23 03:21:03 +0200

    Added function parse(), parseTree() and unittests.

--- src/mll.d
+++ src/mll.d
@@ -2,20 +2,34 @@
  * mll.d - My Little Lisp
  * 
  * Author:  Bystroushaak (bystrousak@kitakitsune.org)
- * Version: 0.0.2
- * Date:    22.06.2012
+ * Version: 0.1.0
+ * Date:    23.06.2012
  * 
  * Copyright: 
  *     This work is licensed under a CC BY.
  *     http://creativecommons.org/licenses/by/3.0/
  * 
  * TODO:
- * 	Preprocesor pro '
+ *  Preprocesor pro '
+ *  Repl smycku s pocitadlem zavorek, teprve pak evalnout.
+ *  skipStringsAndFind(char c) 
+ *
 */
+module mll;
+
 import core.exception;
 
 import std.stdio;		// TODO: Odstranit
 import std.string;
+
+
+/* Exceptions *********************************************************************************************************/
+///
+class ParseException : Exception{
+	this(string msg){
+		super(msg);
+	}
+}
 
 
 
@@ -28,6 +42,12 @@ class LispObject{
 		
 		return "";
 	}
+	
+	public string toLispString(){
+		throw new Exception("Unimplemented : toLispString() for LispObject");
+		
+		return "";
+	}
 }
 
 
@@ -35,37 +55,53 @@ class LispObject{
 class LispArray : LispObject{
 	public LispObject[] members;
 	
+	this(){}
+	
 	this(LispObject[] members){
 		this.members = members;
 	}
 	
-	///
-	public string toString(){
+	/// Returns lisp representation of this list
+	public string toLispString(){
 		string output;
 		
 		foreach(LispObject member; this.members){
-			output ~= member.toString() ~ " ";
+			output ~= member.toLispString() ~ " ";
 		}
 		
-		return "(" ~ output[0 .. $-1] ~ ")";
+		if (output.length >= 2)
+			output.length--;
+		
+		// do not add () to top level object which contains whole dom
+		if (! (members.length == 1 && typeid(this.members[0]) == typeid(LispArray)))
+			output = "(" ~ output ~ ")";
+		
+		return output;
+	}
+
+	/// Returns D representation of this list
+	public string toString(){
+		return std.conv.to!string(members);
 	}
 }
 
 
 /// Object used for representing functions in parsed tree
-class LispOperator : LispObject{
+class LispSymbol : LispObject{
 	private string name;
-	
-	public LispObject[] parameters;
 	
 	this(string name){
 		this.name = name;
 	}
 	
-	
 	///
 	public string toString(){
 		return this.name;
+	}
+	
+	/// Return lisp representation of this object
+	public string toLispString(){
+		return this.toString();
 	}
 }
 
@@ -77,6 +113,9 @@ class LispOperator : LispObject{
  * 
  * Params:
  * 	source = Lisp source, which MUST begins with bracket, which will used as opening bracket for search.
+ *
+ * TODO:
+ *  Add support for builtin strings
 */ 
 private int findMatchingBracket(string source){
 	if (source.length == 0)
@@ -105,17 +144,99 @@ private int findMatchingBracket(string source){
 }
 
 
-LispObject[] cut(string source){
+//TODO: Add support for builtin strings
+private string[] splitSymbols(string symbols){
+	return symbols.split(" ");
+}
+
+
+/**
+ * parseTree
+ * 
+ * This function splits
+ * 
+*/ 
+private LispObject[] parseTree(string source, bool first){
 	LispObject[] output;
+	
+	source = source.strip();
+	
+	if (source.length > 0 && source[0] != '(' && first)
+		throw new ParseException("Invalid expression.\nCorrect expressions starts with '(', not with '" ~ source[0] ~ "'!");
+	
+	// create dom tree
+	LispArray la = new LispArray();
+	while(source.length > 0){
+		if (source[0] == '('){ // recursively parse everything inside expression
+			int expr_length = findMatchingBracket(source);
+			
+			if (expr_length < 0)
+				throw new ParseException("Unclosed expression!\n>" ~ source ~ "< !!");
+			
+			if (expr_length >= 2)
+				la.members ~= parseTree(source[1 .. expr_length - 1], false); // recursion, whee..
+			
+			if (expr_length < source.length)
+				source = source[expr_length + 1 .. $];
+			else
+				source.length = 0;
+		}else{
+			string tmp;
+			
+			// to tmp save symbols in list until next expression (>>xe xa<< (exp)) -> tmp = xe xa
+			int next_expr = source.indexOf('(');
+			if (next_expr > 0){
+				tmp = source[0 .. next_expr];
+				
+				if (next_expr < source.length)
+					source = source[next_expr .. $];
+				else
+					source.length = 0;
+			}else{
+				tmp = source;
+				source.length = 0;
+			}
+			
+			// append symbols to the list
+			foreach(string symbol; tmp.splitSymbols()){
+				symbol = symbol.strip();
+				
+				if (symbol.length > 0)
+					la.members ~= new LispSymbol(symbol);
+			}
+		}
+		
+	}
+	
+	output ~= la;
 	
 	return output;
 }
 
+public LispArray parse(string source){
+	return cast(LispArray) parseTree(source, true)[0];
+}
 
 
 unittest{
+	// findMatchingBracket
 	assert(findMatchingBracket("(cons 1 (cons (q (2 3)) 4)") == -1);
 	assert(findMatchingBracket("(cons 1 (cons (q (2 3)) 4))") == "(cons 1 (cons (q (2 3)) 4))".length);
+	
+	// splitSymbols
+	assert(splitSymbols("a b") == ["a", "b"]);
+	
+	// parse
+	void testParseWithBothStrings(string expr, string d_result, string lisp_result){
+		assert(parse(expr).toString()     == d_result);
+		assert(parse(expr).toLispString() == lisp_result);
+	}
+	testParseWithBothStrings("()", "[[]]", "()");
+	testParseWithBothStrings("(a)", "[[a]]", "(a)");
+	testParseWithBothStrings("(a (b))", "[[a, [b]]]", "(a (b))");
+	testParseWithBothStrings("(a  (     b  )   	)", "[[a, [b]]]", "(a (b))");
+	testParseWithBothStrings("((((()))))", "[[[[[[]]]]]]", "()");
+	testParseWithBothStrings("(a (b (c (d (e)))))", "(a (b (c (d (e)))))", "[[a, [b, [c, [d, [e]]]]]]");
 }
 
 
